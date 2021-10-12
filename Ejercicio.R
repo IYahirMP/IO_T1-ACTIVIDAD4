3e7a371dbb3524c6de79bd45ec39bad3b788d985
IYahirMP <80314907+IYahirMP@users.noreply.github.com>
2021-10-11 22:24:46 -0500

    Se añade y muestra la matriz restricciones_izquierdo

--- Ejercicio.R
+++ Ejercicio.R
@@ -3,3 +3,12 @@ library(lpSolve)
 
 #Se guardan en una matriz los coeficientes de la función objetivo
 funcion_objetivo <- c(19, 23)
+#Se muestran los coeficientes de la función objetivo
+funcion_objetivo
+
+#Asignar a una matriz los coeficientes del lado izquierdo de las desigualdades
+#2x1 + 1.5x2 <= 650
+#4x1 + 6x2 <= 70
+restricciones_izquierdo <- matrix(c(2, 1.5, 4, 6), nrow = 2, byrow = T)
+# Se muestra el valor de la matriz
+restricciones_izquierdo
